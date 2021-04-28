f454ae5b58ebe08130b8f2fe4150d3c71628ce6b
Gramkraxor <gram@krax.dev>
2021-04-28 11:14:08 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ import setuptools
 
 long_description = Path('README.md').read_text()
 
-# https://packaging.python.org/guides/distributing-packages-using-setuptools/
 setuptools.setup(
     name='mwot',
     version='0.0.0.dev0',
@@ -13,9 +12,8 @@ setuptools.setup(
     description='An esolang',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    keywords=['esolang', 'esoteric language'],
+    keywords=['esolang', 'esoteric language', 'brainfuck'],
     classifiers=[
-        'Development Status :: 1 - Planning',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3 :: Only',
@@ -28,7 +26,6 @@ setuptools.setup(
     packages=setuptools.find_packages(where='src'),
     package_dir={'': 'src'},
     python_requires='>=3.8',
-    install_requires=['setuptools'],
     entry_points={
         'console_scripts': [
             'mwot = mwot.cli:main',
