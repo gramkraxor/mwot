a0d10aaf2495b90ddf3bde4d17f769b8d98cae4e
Gramkraxor <gram@krax.dev>
2021-04-28 10:49:22 -0500

    Add compiling shortcut functions

--- src/mwot/__init__.py
+++ src/mwot/__init__.py
@@ -4,3 +4,20 @@ from . import binary
 from . import brainfuck
 from .compiler import mwot_to_bits
 from .exceptions import CompilerError, MWOTError
+
+
+def mwot_to_bf(mwot):
+    """Convert MWOT source to brainfuck."""
+    return bytes(mwot_to_bf_chars(mwot))
+
+def mwot_to_bf_chars(mwot):
+    """Convert MWOT source to brainfuck instructions."""
+    return brainfuck.from_bits(mwot_to_bits(mwot))
+
+def mwot_to_binary(mwot):
+    """Convert MWOT source to binary."""
+    return bytes(mwot_to_binary_chars(mwot))
+
+def mwot_to_binary_chars(mwot):
+    """Convert MWOT source to binary bytes."""
+    return bytes.from_bits(mwot_to_bits(mwot))
