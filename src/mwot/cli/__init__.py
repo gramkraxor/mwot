684ea42e56dd6f5f027f9bfffb580123884cf94b
Gramkraxor <gram@krax.dev>
2021-05-07 17:19:20 -0500

    Define shebang helpers properly

--- src/mwot/cli/__init__.py
+++ src/mwot/cli/__init__.py
@@ -1,9 +1,19 @@
 import sys
 
 
-def main():
+def main(args=None, prefix_args=()):
+    if args is None:
+        args = sys.argv[1:]
+    args = [*args, *prefix_args]
     print('Coming soon!', file=sys.stderr)
     sys.exit(1)
 
 
-mwot_i_bf = mwot_x_bf = main
+def mwot_i_bf(args=None):
+    """Shebang helper to run brainfuck MWOT."""
+    main(args, prefix_args=['-ib'])
+
+
+def mwot_x_bf(args=None):
+    """Shebang helper to run brainfuck."""
+    main(args, prefix_args=['-xb'])
